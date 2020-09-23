9076ac81c77952d9e8fc13cef16412b561914630
zhim57 <65269162+zhim57@users.noreply.github.com>
2020-09-23 19:41:35 -0400

    fixing connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Password1",
-  database: "burger_db"
-});
+var connection ;
+if ( typeof process.env.JAWSDB_URL !== "undefined") {
+connection = mysql.createConnection( process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password1",
+    database: "burger_db"
+  });
+}
+
+
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
